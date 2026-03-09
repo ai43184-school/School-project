fc1b22e871394296f03e3f9ff5cd4a7920de8e9f
ai43184-school <ai43184@stu.alaschools.org>
2026-03-09 10:49:57 -0700

    lil prog

--- only-way-is-up/Scripts/player.gd
+++ only-way-is-up/Scripts/player.gd
@@ -1,4 +1,10 @@
 extends RigidBody2D
 
+const speed = 10
 
-const SPEED := 50
+func Launch():
+	var mouse_pos = get_viewport().get_mouse_position()
+	
+
+func _physics_process(delta: float) -> void:
+	Launch()
