e01e58381c1850ea25c55f0ea31a1187ee0a7b82
ai43184-school <ai43184@stu.alaschools.org>
2026-03-10 10:52:09 -0700

    idk at this point

--- only-way-is-up/Scripts/player.gd
+++ only-way-is-up/Scripts/player.gd
@@ -1,10 +1,11 @@
 extends RigidBody2D
 
-const speed = 10
+var velocity : Vector2 = Vector2.ZERO
+var direction : Vector2 = Vector2.ZERO
+var speed: float = 1200.0
 
-func Launch():
-	var mouse_pos = get_viewport().get_mouse_position()
-	
 
 func _physics_process(delta: float) -> void:
-	Launch()
+	direction = (get_global_mouse_position() - position)
+	if Input.is_action_pressed("Launch"):
+		velocity = direction * speed
