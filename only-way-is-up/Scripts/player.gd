668bd84eefccfd40afccf85b231a338643fdee0f
ai43184-school <ai43184@stu.alaschools.org>
2026-03-11 11:01:32 -0700

    PLAYER MOVEMENT#1
    FINALLY

--- only-way-is-up/Scripts/player.gd
+++ only-way-is-up/Scripts/player.gd
@@ -2,10 +2,19 @@ extends RigidBody2D
 
 var velocity : Vector2 = Vector2.ZERO
 var direction : Vector2 = Vector2.ZERO
-var speed: float = 1200.0
+var speed: float = 2
 
 
-func _physics_process(delta: float) -> void:
+func Launch():
 	direction = (get_global_mouse_position() - position)
 	if Input.is_action_pressed("Launch"):
-		velocity = direction * speed
+		Engine.time_scale = 0.25
+		look_at(get_global_mouse_position())
+	elif Input.is_action_just_released("Launch"):
+		linear_velocity = velocity
+		apply_impulse(direction * speed)
+	else:
+		Engine.time_scale = 1
+
+func _physics_process(delta: float) -> void:
+	Launch()
