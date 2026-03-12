7bc515b312bda17168481f3a4b3dab2fc5507688
ai43184-school <ai43184@stu.alaschools.org>
2026-03-12 11:02:35 -0700

    Adding Enemies

--- only-way-is-up/Scripts/player.gd
+++ only-way-is-up/Scripts/player.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 var velocity : Vector2 = Vector2.ZERO
 var direction : Vector2 = Vector2.ZERO
 var speed: float = 2
+var can_launch = true
 
 
 func Launch():
@@ -13,8 +14,20 @@ func Launch():
 	elif Input.is_action_just_released("Launch"):
 		linear_velocity = velocity
 		apply_impulse(direction * speed)
+		can_launch = false
+		Engine.time_scale = 1
 	else:
 		Engine.time_scale = 1
 
+func KilledEnemy():
+	if GameManager.enemydeath:
+		apply_impulse(Vector2.UP)
+
+
 func _physics_process(delta: float) -> void:
-	Launch()
+	if can_launch:
+		Launch()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	can_launch = true
