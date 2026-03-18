321f810836672f46ea951b7edaa5f348726afbb4
RONGAMES2 <adrianighere@gmail.com>
2026-03-18 07:19:59 -0400

    Finished Enemies

--- only-way-is-up/Scripts/enemy_1.gd
+++ only-way-is-up/Scripts/enemy_1.gd
@@ -4,6 +4,6 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		GameManager.enemydeath.emit()
+		GameManager.enemydeath.emit(1)
 		GameManager.can_launch = true
 		queue_free()
--- only-way-is-up/Scripts/player.gd
+++ only-way-is-up/Scripts/player.gd
@@ -25,5 +25,8 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	GameManager.can_launch = true
-	if body.is_in_group("Enemy"):
-		apply_impulse(Vector2.UP*100)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Enemy"):
+		linear_velocity += Vector2.UP*1200
