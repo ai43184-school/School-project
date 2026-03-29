177ad6c671bf893cce945a14eab85343465e6eeb
RONGAMES2 <adrianighere@gmail.com>
2026-03-29 11:02:10 -0700

    cant finish anything kms

--- only-way-is-up/Scripts/game_manager.gd
+++ only-way-is-up/Scripts/game_manager.gd
@@ -1,33 +1,7 @@
 extends Node
 
 #enemies
-@warning_ignore("unused_signal")
 signal enemydeath
 
 #player movement
 var can_launch = true
-
-# Preload the enemy scene
-var enemy_scene = preload("res://Scenes/Enemy1.tscn")
-
-# Set a seed for reproducibility
-@export var spawn_seed = randi_range(0,5000)
-var rng = RandomNumberGenerator.new()
-
-func _ready():
-	# Seed the RNG
-	rng.seed = spawn_seed
-	spawn_enemies(1000) # Spawn 10 enemies
-
-func spawn_enemies(amount):
-	for i in range(amount):
-		var enemy = enemy_scene.instantiate()
-		
-		# Generate random position within a specific area
-		var spawn_x = rng.randf_range(0, 1000)
-		var spawn_y = rng.randf_range(0, 600)
-		enemy.position = Vector2(spawn_x, spawn_y)
-		
-		# Add enemy to the scene
-		add_child(enemy)
-		
--- only-way-is-up/Scripts/player.gd
+++ only-way-is-up/Scripts/player.gd
@@ -1,8 +1,18 @@
 extends RigidBody2D
 
+@export var camera_2d: Camera2D
+
 var velocity : Vector2 = Vector2.ZERO
 var direction : Vector2 = Vector2.ZERO
 var speed: float = 2
+
+# spawning enemies
+@onready var enemy = preload("res://Scenes/Enemy1.tscn")
+
+var enemies = ["normalEnemy", "spikeEnemy"]
+
+var distance: float
+var distanceUsed: float
 
 
 func Launch():
@@ -14,14 +24,40 @@ func Launch():
 		linear_velocity = velocity
 		apply_impulse(direction * speed)
 		Engine.time_scale = 1
-		GameManager.can_launch = false
+		GameManager.can_launch = true
 	else:
 		Engine.time_scale = 1
 
 func _physics_process(delta: float) -> void:
 	if GameManager.can_launch:
 		Launch()
+	
+	
+	if (distance < position.x + 50):
+		distance = position.x + 50
+	
+	var distToGo: float = floor(distance - distanceUsed)
+	
+	if (distanceUsed < distance && distToGo > 4):
+		distanceUsed = distance
+		SpawnEnemy()
+	
+	
+	
 
+func SpawnEnemy():
+	var enemyToSpawn = SelectEnemyToSpawn()
+	
+	var yPos: float = floor(abs(randf_range(0.0,1.0)-randf_range(0.0,1.0)) * (1+100 - (-2)) + (-2))
+	var posToSpawnEnemy: Vector2 = Vector2(distance,yPos)
+	
+	enemyToSpawn.position = posToSpawnEnemy
+	print(posToSpawnEnemy)
+	add_sibling(enemyToSpawn)
+
+func SelectEnemyToSpawn():
+	var enemy_scene = enemy.instantiate()
+	return enemy_scene
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	GameManager.can_launch = true
@@ -29,4 +65,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Enemy"):
+		linear_velocity = -linear_velocity / 4
 		linear_velocity += Vector2.UP*500
