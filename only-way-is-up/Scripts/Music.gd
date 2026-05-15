5e678fccb6fa00e1c400b5833dfc5b77c59ee82a
ai43184-school <ai43184@stu.alaschools.org>
2026-05-15 11:25:02 -0700

    hkgdkjhbflwjhb/kf;wbhe

--- only-way-is-up/Scripts/Music.gd
+++ only-way-is-up/Scripts/Music.gd
@@ -1,10 +1,12 @@
 extends AudioStreamPlayer2D
 
+var tween = create_tween()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	playing = true
-	
+
 
 func _process(delta: float) -> void:
 	if GameManager.score == 0:
@@ -15,7 +17,7 @@ func _process(delta: float) -> void:
 		stream.set_sync_stream_volume(4, -60)
 	if GameManager.score == 1000:
 		stream.set_sync_stream_volume(0, -60)
-		stream.set_sync_stream_volume(1, 0)
+		stream.get_sync_stream_volume(1, 0)
 		stream.set_sync_stream_volume(2, -60)
 		stream.set_sync_stream_volume(3, -60)
 		stream.set_sync_stream_volume(4, -60)
